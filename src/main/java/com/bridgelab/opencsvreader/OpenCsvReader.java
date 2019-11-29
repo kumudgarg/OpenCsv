580752372cda0326e07335f0361c7dad9606ddbe
kumudgarg <kumud50garg@gmail.com>
2019-11-29 11:00:13 +0530

    read csv file into String array

---
+++ src/main/java/com/bridgelab/opencsvreader/OpenCsvReader.java
@@ -0,0 +1,31 @@
+package com.bridgelab.opencsvreader;
+
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class OpenCsvReader {
+    private static final String SAMPLE_CSV_FILE_PATH = "./OpenCsv.csv";
+
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+                CSVReader csvReader = new CSVReader(reader);
+        ) {
+            String[] nextRecord;
+            while ((nextRecord = csvReader.readNext()) != null) {
+                System.out.println("Name : " + nextRecord[0]);
+                System.out.println("Email : " + nextRecord[1]);
+                System.out.println("Phone : " + nextRecord[2]);
+                System.out.println("Country : " + nextRecord[3]);
+                System.out.println("===========================");
+            }
+        }
+
+    }
+}
