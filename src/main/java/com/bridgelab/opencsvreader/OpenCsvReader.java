fb5935cb2b578a3bf8b4a0c041dab5c48fc925a7
kumudgarg <kumud50garg@gmail.com>
2019-11-29 12:02:34 +0530

    Refactor : Read all records at once by taking list

--- src/main/java/com/bridgelab/opencsvreader/OpenCsvReader.java
+++ src/main/java/com/bridgelab/opencsvreader/OpenCsvReader.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCsvReader {
     private static final String SAMPLE_CSV_FILE_PATH = "./OpenCsv.csv";
@@ -17,12 +18,12 @@ public class OpenCsvReader {
                 Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
                 CSVReader csvReader = new CSVReader(reader);
         ) {
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-                System.out.println("Name : " + nextRecord[0]);
-                System.out.println("Email : " + nextRecord[1]);
-                System.out.println("Phone : " + nextRecord[2]);
-                System.out.println("Country : " + nextRecord[3]);
+            List<String[]> records = csvReader.readAll();
+            for (String[] record : records) {
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
                 System.out.println("===========================");
             }
         }
