c93b7f7d881aa502588538de97219cc37e357059
kumudgarg <kumud50garg@gmail.com>
2019-11-29 13:03:33 +0530

    Main program to parse csv into pojo class

---
+++ src/main/java/com/bridgelab/opencsvreader/OpenCSVReadAndParseToBean.java
@@ -0,0 +1,41 @@
+package com.bridgelab.opencsvreader;
+
+import com.opencsv.CSVIterator;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class OpenCSVReadAndParseToBean {
+    private static final String SAMPLE_CSV_FILE_PATH = "./OpenCsv.csv";
+
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+
+        ) {
+            CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVUser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
+            while (csvUserIterator.hasNext()){
+                CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("Phone : " + csvUser.getPhone());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("===========================");
+            }
+        }
+
+    }
+}
