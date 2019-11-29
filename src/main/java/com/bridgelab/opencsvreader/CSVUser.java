69cc5dafca1da4bd4789c58e9a3a846215ac9223
kumudgarg <kumud50garg@gmail.com>
2019-11-29 12:17:55 +0530

    Defined POJO class to read csv file directly

---
+++ src/main/java/com/bridgelab/opencsvreader/CSVUser.java
@@ -0,0 +1,59 @@
+package com.bridgelab.opencsvreader;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+
+    @CsvBindByName(column = "phone")
+    private String phone;
+
+    @CsvBindByName
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "CSVUser{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
